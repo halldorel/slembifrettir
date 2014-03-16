3bbb9d18363486fd05db581ec8b6d891616f7673
Halldór Eldjárn <halldor@eldjarn.net>
2014-03-16 13:14:47 +0000

    Added visir.is to scraper

--- scraper.py
+++ scraper.py
@@ -5,9 +5,9 @@ import json
 
 # dv.is
 
-site_data = {'dv.is' : [], 'mbl.is' : []}
+site_data = {'dv.is' : [], 'mbl.is' : [], 'visir.is' : []}
 
-for subpage in range(1, 2):
+for subpage in range(1, 10):
 	r = requests.get('http://www.dv.is/frettir/' + str(subpage))
 	data = r.text
 	soup = BeautifulSoup(data)
@@ -17,7 +17,7 @@ for subpage in range(1, 2):
 		headlines = soup[0].find_all('h1')
 		for hl in headlines:
 			site_data['dv.is'].append(hl.get_text())
-		print "Woohoo! - dv.is - " + str(subpage)
+		print "Woohoo! - dv.is - " + str(subpage) + ", count:" + str(len(headlines))
 	except:
 		print "Dammit! - dv.is - " + str(subpage)
 		pass
@@ -37,10 +37,25 @@ try:
 	
 	for hl in headlines:
 		site_data['mbl.is'].append(hl.get_text())
-	print "Woohoo! - mbl.is"
+	print "Woohoo! - mbl.is" + ", count:" + str(len(headlines))
 
 except:
 	print "Dammit! - mbl.is"
+	pass
+
+# visir.is
+
+r = requests.get('http://www.visir.is/section/FRETTIR')
+data = r.text
+soup = BeautifulSoup(data)
+
+try:
+	headlines = soup.find_all('h3')
+	for hl in headlines:
+		site_data['visir.is'].append(hl.get_text())
+	print "Woohoo! - visir.is" + ", count:" + str(len(headlines))
+except:
+	print "Dammit! - visir.is"
 	pass
 
 f = open('scrape_dump.txt', 'w')
